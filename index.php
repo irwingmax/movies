23f04c4289f94978e1bcfe0c5742d732bf1bb187
irwing19 <irwing.1911@gmail.com>
2017-12-04 13:36:36 -0200

    create new pages

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Página Inicial</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Filmes</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="adiciona-itens.php">Adicionar novos filmes</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+	<h2>Lista de filmes</h2>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Filme</th>
+				<th>Categoria</th>
+				<th>Duração</th>
+				<th>Censura</th>
+				<th></th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+				<tr>
+
+				</tr>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
