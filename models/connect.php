9b48574da36281eea12e2f4a663f16baf522b37a
irwing19 <irwing.1911@gmail.com>
2018-01-30 15:30:06 -0200

    DataBase - Updating connect.php file Select introduction. The implementation of this file is for testing only

--- models/connect.php
+++ models/connect.php
@@ -1,13 +1,29 @@
 <?php
-$servername = "localhost:3306";
+$servername = "localhost";
 $username = "root";
-$password = "";
+$password = "irwing";
+$dbname = "db_movies";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+echo "Connected\n";
+
+
+
+$sql = "SELECT movieID, Title, Director FROM tb_movies";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "movieID: " . $row["movieID"]. " - Title: " . $row["Title"]. " - Director " . $row["Director"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
