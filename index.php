c8ed97aaefd0e3f175a4719ecbe28801a425f30b
irwing19 <irwing.1911@gmail.com>
2018-01-31 15:21:41 -0200

    Refactoring - Remove twig-config.php The twig settings were placed in index.php.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-require __DIR__ . "/twig-config.php";
+require __DIR__ . '/vendor/autoload.php';
+
+$viewFolder = __DIR__ . "/views";
+$loader = new Twig_Loader_Filesystem($viewFolder);
+
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index.html');
