a7a4af275d98d755abbc1575177d7768b286da4e
irwing19 <irwing.1911@gmail.com>
2018-03-14 14:43:08 -0300

    Refactoring: remove functions The functions were removed and placed in a single file. Now for the other php files of the application, only the functions will be called.

--- adiciona-filmes.php
+++ adiciona-filmes.php
@@ -1,25 +1,4 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/functions.php';
 
-use Irwing\Movies\controllers\RequestData;
-use Irwing\Movies\controllers\InsertData;
-
-function insertData()
-{
-	$objRequest = new RequestData();
-    $insert = new InsertData();
-    $insert->insert($objRequest->getTitulo(), $objRequest->getDiretor(), $objRequest->getAtor());
-}
-
-function twigAdcionaFilmes()
-{
-    $viewFolder = __DIR__ . "/views";
-    $loader = new Twig_Loader_Filesystem($viewFolder);
-
-    $twig = new Twig_Environment($loader);
-
-    echo $twig->render('adiciona-filmes.html');
-}
-
-insertData();
 twigAdcionaFilmes();
--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-use Irwing\Movies\Controllers\SelectData;
-
-function showData()
-{
-	$objSelect = new SelectData();
-    $data = $objSelect->getData();
-
-    return $data;
-}
-
-function twigIndex()
-{
-    $viewFolder = __DIR__ . "/views";
-    $loader = new Twig_Loader_Filesystem($viewFolder);
-    $twig = new Twig_Environment($loader);
-
-    echo $twig->render('index.html', array('postsMovies'=>showData()));
-
-}
+require __DIR__ . '/functions.php';
 
 twigIndex();
