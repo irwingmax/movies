4a8fc54ea815b6f003d71ffea81e41a60ce1514b
irwing19 <irwing.1911@gmail.com>
2018-04-02 00:00:27 -0300

    remove blank space

--- app/models/UpdateData.php
+++ app/models/UpdateData.php
@@ -4,15 +4,14 @@ namespace Irwing\Movies\Models;
 
 class UpdateData
 {
-
     public function update($id, $newTitle, $newDirector, $newActor)
     {
-            $objectConect = new Connect();
-            $objectConect->connectar();
-            $updateQuery = "UPDATE tb_movies 
-                            SET Title='$newTitle', Director='$newDirector', Actor='$newActor' 
-                            WHERE movieID = '$id'";
-            $updateStmt = $objectConect->getConnection()->prepare($updateQuery);
-            $updateStmt->execute();
+        $objectConect = new Connect();
+        $objectConect->connectar();
+        $updateQuery = "UPDATE tb_movies 
+                        SET Title='$newTitle', Director='$newDirector', Actor='$newActor' 
+                        WHERE movieID = '$id'";
+        $updateStmt = $objectConect->getConnection()->prepare($updateQuery);
+        $updateStmt->execute();
     }
 }
