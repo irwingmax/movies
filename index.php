0d7e99be23321d4f009350a0d6428033caeb0e20
irwingmax <irwing.1911@gmail.com>
2018-09-21 11:42:30 -0300

    changing require and including the correct method for the layout call.

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/functions.php';
 
-use Irwing\Movies\Twig\TemplateRendering;
-
-$objectTwig = new TemplateRendering();
-$objectTwig->rendering('index.html');
+twigIndex();
 
